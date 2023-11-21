9d923ad86bf1cb31272d404f857538fc51958341
Angela Hempkmeier <angela.da.silva@avanade.com>
2023-11-21 14:32:08 -0300

    test(cypress: email validation)

--- first-cypress-register-page/cypress/e2e/firsttest.cy.js
+++ first-cypress-register-page/cypress/e2e/firsttest.cy.js
@@ -5,10 +5,27 @@ describe('register page', () => {
     .should('have.text', 'Cypress Test for a Simple Page Application');
   });
 
-  it('the name is completed and it\'s not empty', () => {
+  it('the name is filled and it\'s not empty', () => {
     cy.visit('http://127.0.0.1:5500/');
     cy.wait(4000);
-    cy.get('#name').should('exist').and('have.value', 'angela');
+    cy.get('#name').should('exist');
+    cy.get('#name').invoke('val').should('not.be.empty');
+  });
 
+  it('the last name is filled and it\'s not empty', () => {
+    cy.visit('http://127.0.0.1:5500/');
+    cy.wait(4000);
+    cy.get('#lastname').should('exist');
+    cy.get('#lastname').invoke('val').should('not.be.empty');
+  });
+
+  it('validation of the email input', () => {
+    cy.visit('http://127.0.0.1:5500/');
+    cy.get('#email').should('exist');
+    cy.get('#email').invoke('val').should('not.be.empty');
+    cy.get('#email').then(($input) => {
+      const enteredEmail = $input.val();
+      expect(enteredEmail).to.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/);
+    });
   });
 })
--- first-cypress-register-page/index.js
+++ first-cypress-register-page/index.js
@@ -1,1 +1,3 @@
 const nameInput = document.getElementById('name').value = 'angela';
+const lastNameInput = document.getElementById('lastname').value = 'hempkmeier';
+const emailInput = document.getElementById('email').value = 'angela@outlook.com';
